5e5f3d125462154f796d25f1cd07b9a4ec1c1d18
wjlee <000jun2626@gmail.com>
2025-08-30 06:09:33 +0900

    init

---
+++ src/main/java/kr/ac/hansung/cse/strikezonevr/StrikeZoneVrApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.hansung.cse.strikezonevr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrikeZoneVrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrikeZoneVrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/hansung/cse/strikezonevr/StrikeZoneVrApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.hansung.cse.strikezonevr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrikeZoneVrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
